26a30bc805976e4799c6ad963d6ca8a52d2f651e
Jacob Quon <jacob.quon@alleninstitute.org>
2023-09-11 16:33:47 -0700

    Added support for meta's SAM

--- code/sawg/segmentation.py
+++ code/sawg/segmentation.py
@@ -502,8 +502,242 @@ class BaysorSegmentationResult(SegmentationResult):
 
         return self._cell_ids
 
+class MetaSegmentationMethod(SegmentationMethod):
+    """Implements 2D or 3D Meta SegmentAnything segmentation on SpotTable
+
+    Will automatically segment from images attached to the SpotTable or
+    generate an image from total mRNA.
+    
+    options = {
+        'region': ((xmin, xmax), (ymin, ymax)),  # or None for whole table
+        'images': {
+            'nuclei': 'DAPI',
+            'cyto': 'total_mrna',
+        },
+        'meta_options': {
+            points_per_side: 32,
+            points_per_batch: 64,
+            pred_iou_thresh: 0.88,
+            stability_score_thresh: 0.95,
+            stability_score_offset: 1.0,
+            box_nms_thresh: 0.7,
+            crop_n_layers: 0,
+            crop_nms_thresh: 0.7,
+            crop_overlap_ratio: 512 / 1500,
+            crop_n_points_downscale_factor: 1,
+            point_grids: None,
+            min_mask_region_area: 0,
+            output_mode: "binary_mask",
+        },
+    }
+    """
+    def __init__(self, options):
+        super().__init__(options)
+
+    def run(self, spot_table):
+        spot_table = self._get_spot_table(spot_table)
+
+        from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+        sam_checkpoint = self.options['meta_options']['sam_checkpoint']#"sam_vit_h_4b8939.pth"
+        model_type = "vit_h"
+        device = "cuda"
+        sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+        sam.to(device=device)
+        mask_generator = SamAutomaticMaskGenerator(sam)
+
+        # collect images
+        images = {}
+        if 'nuclei' in self.options['images']:
+            img_spec = self.options['images']['nuclei']
+            images['nuclei'] = self._read_image_spec(img_spec, spot_table, px_size=self.options['px_size'], images=images)
+        if 'cyto' in self.options['images']:
+            img_spec = self.options['images']['cyto']
+            img = self._read_image_spec(img_spec, spot_table, px_size=self.options['px_size'], images=images)
+            images['cyto'] = img
+        self.images = images
+
+        if len(self.images) == 1:
+            image_type = list(self.images.keys())[0]
+            image_data = np.stack((self.images[image_type].get_data().mean(axis=0), self.images[image_type].get_data().mean(axis=0), self.images[image_type].get_data().mean(axis=0)), axis=-1)
+        elif len(self.images) == 2:
+            min_x = min(self.images['cyto'].shape[1], self.images['nuclei'].shape[1])
+            min_y = min(self.images['cyto'].shape[2], self.images['nuclei'].shape[2])
+            image_data = np.stack((self.images['cyto'].get_data().mean(axis=0)[:min_x, :min_y], self.images['nuclei'].get_data().mean(axis=0)[:min_x, :min_y], np.zeros((min_x, min_y))), axis=-1)
+        else:
+            raise ValueError('Unexpected number of image layers')
+
+        image_data = (np.ma.divide(image_data - image_data.min(axis=(0,1)), image_data.max(axis=(0,1)) - image_data.min(axis=(0,1))).filled(0) * 255).astype(np.uint8) # Must transform to int for SAM to work
+        masks = mask_generator.generate(image_data)
+
+        # Deal with massive spike for full image mask
+        area_check_list = sorted([x['area'] for x in masks])[::-1]
+        if area_check_list[0] >= 25 * area_check_list[1]:
+            for i, x in enumerate(masks):
+                if x['area'] == area_check_list[0]:
+                    full_image_idx = i
+            masks.pop(full_image_idx)
+
+        result = MetaSegmentationResult(
+            method=self, 
+            input_spot_table=spot_table,
+            meta_output={
+                'masks': masks
+            },
+            image_transform=list(images.values())[0].transform,
+        )
+
+        return result
 
 
+    def _read_image_spec(self, img_spec, spot_table, px_size, images):
+        """Return an Image to be used in segmentation based on img_spec:
+        
+        - An Image instance is returned as-is
+        - "total_mrna" returns an image generated from spot density
+        - Any other string returns an image channel attached to the spot table
+        - {'channel': channel, 'frame': int} can be used to select a single frame
+        - {'channel': 'total_mrna', 'gauss_kernel': (1, 3, 3), 'median_kernel': (2, 10, 10)} can be ued to configure total mrna image generation
+        """
+        # optionally, cyto image may be generated from spot table total mrna        
+        if img_spec is None or isinstance(img_spec, ImageBase):
+            return img_spec
+        if isinstance(img_spec, str):
+            img_spec = {'channel': img_spec}
+        if not isinstance(img_spec, dict):
+            raise TypeError(f"Bad image spec: {img_spec}")
+
+        if img_spec['channel'] == 'total_mrna':
+            opts = img_spec.copy()
+            opts.pop('channel')
+            opts.update(self._suggest_image_spec(spot_table, px_size, images))
+            return self.get_total_mrna_image(spot_table, **opts)
+        else:
+            return spot_table.get_image(channel=img_spec['channel'], frame=img_spec.get('frame', None))
+
+
+    def _suggest_image_spec(self, spot_table, px_size, images):
+        """Given a pixel size, return {'image_shape': shape, 'image_transform': tr} covering the entire area of spot_table.
+        If any images are already present, use those as templates instead.
+        """
+        if len(images) > 0:
+            img = list(images.values())[0]
+            return {'image_shape': img.shape[:3], 'image_transform': img.transform}
+
+        bounds = np.array(spot_table.bounds())
+        scale = 1 / px_size
+        shape = np.ceil((bounds[:, 1] - bounds[:, 0]) * scale).astype(int)
+        
+        tr_matrix = np.zeros((2, 3))
+        tr_matrix[0, 0] = tr_matrix[1, 1] = scale
+        tr_matrix[:, 2] = -scale * bounds[:, 0]
+        image_tr = ImageTransform(tr_matrix)
+        
+        return {'image_shape': (1, shape[0], shape[1]), 'image_transform': image_tr}
+
+
+    def get_total_mrna_image(self, spot_table, image_shape:tuple, image_transform:ImageTransform, gauss_kernel=(1, 3, 3), median_kernel=(2, 10, 10)):
+            
+        density_img = np.zeros(image_shape[:3], dtype='float32')
+
+        spot_px = self.map_spots_to_img_px(spot_table, image_transform=image_transform, image_shape=image_shape)
+        n_planes = density_img.shape[0]
+        for i in range(n_planes):
+            z_mask = spot_px[..., 0] == i
+            x = spot_px[z_mask, 1]
+            y = spot_px[z_mask, 2]
+            bins = [
+                # is this correct? test on rectangular tile
+                np.arange(image_shape[1]+1),
+                np.arange(image_shape[2]+1),
+            ]
+            density_img[i] = np.histogram2d(x, y, bins=bins)[0]
+
+        import scipy.ndimage
+        # very sensitive to these parameters :/
+        density_img = scipy.ndimage.gaussian_filter(density_img, gauss_kernel)
+        density_img = scipy.ndimage.median_filter(density_img, median_kernel)
+        
+        return Image(density_img[..., np.newaxis], transform=image_transform, channels=['Total mRNA'], name=None)
+
+    def map_spots_to_img_px(self, spot_table:SpotTable, image:Image|None=None, image_transform:ImageTransform|None=None, image_shape:tuple|None=None):
+        """Map spot table (x, y, z) positions to image (frame, row, col). 
+
+        Optionally, provide the *image_transform* and *image_shape* instead of *image*.
+        """
+        if image is not None:
+            assert image_transform is None and image_shape is None
+            image_shape = image.shape
+            image_transform = image.transform
+
+        spot_xy = spot_table.pos[:, :2]
+        spot_px_rc = np.floor(image_transform.map_to_pixels(spot_xy)).astype(int)
+
+        # for this dataset, z values are already integer index instead of um
+        if spot_table.z is None:
+            spot_px_z = np.zeros(len(spot_table), dtype=int)
+        else:
+            spot_px_z = spot_table.z.astype(int)
+
+        # some spots may be a little past the edge of the image; 
+        # just clip these as they'll be discarded when tiles are merged anyway
+        spot_px_rc[:, 0] = np.clip(spot_px_rc[:, 0], 0, image_shape[1]-1)
+        spot_px_rc[:, 1] = np.clip(spot_px_rc[:, 1], 0, image_shape[2]-1)
+        
+        return np.hstack([spot_px_z[:, np.newaxis], spot_px_rc])
+
+
+class MetaSegmentationResult(SegmentationResult):
+    def __init__(self, method:SegmentationMethod, input_spot_table:SpotTable, meta_output:dict, image_transform:ImageTransform):
+        super().__init__(method, input_spot_table)
+        self.meta_output = meta_output
+        self.image_transform = image_transform
+        self._cell_ids = None
+
+    @property
+    def cell_ids(self):
+        """Array of segmented cell IDs for each spot in the table
+        """
+        # use segmented masks to assign each spot to a cell
+        if self._cell_ids is None:
+            spot_table = self.input_spot_table
+            mask_img = self.mask_image
+            spot_px = self.method.map_spots_to_img_px(spot_table, mask_img)
+
+            # assign segmented cell IDs to a new table
+            masks = mask_img.get_data()
+            if self.meta_output['masks'][0]['segmentation'].ndim == 2:
+                # 2D mask
+                self._cell_ids = masks[0, spot_px[:,1], spot_px[:,2]]
+            else:
+                # 3D mask
+                self._cell_ids = masks[spot_px[:,0], spot_px[:,1], spot_px[:,2]]
+        
+        return self._cell_ids
+
+    @property
+    def mask_image(self):
+        """Return Image instance containing mask data and pixel transform
+        """        
+        # annotate segmentation mask with spot-to-pixel transform
+        masks = self.meta_output['masks']
+        # Pull out the actual image data
+        img = np.ones((masks[0]['segmentation'].shape[0], masks[0]['segmentation'].shape[1]), dtype=np.int64)
+        for i, mask in enumerate(masks):
+            img[mask['segmentation']] = i
+
+        if img.ndim == 2:
+            # add frame and channel axes back
+            img = img[np.newaxis, ..., np.newaxis]
+        else:
+            # add channel axis back
+            img = img[..., np.newaxis]
+        return Image(data=img, transform=self.image_transform, channels=['Meta Mask'], name=None)
+        
+    def show(self, ax):
+        """Display segmentation result in matplotlib axes
+        """
+        
+        
 def dilate_labels(img, radius):
     """Dilate labeled regions of an image.
 
