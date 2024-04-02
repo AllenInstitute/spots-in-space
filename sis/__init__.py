0a5ce001fe526ade06b3f336dda9d3d19090650e
Brian Long <brianl@alleninstitute.org>
2024-04-02 12:57:55 -0700

    remove * import of spatial_dataset

--- sis/__init__.py
+++ sis/__init__.py
@@ -1,8 +1,8 @@
-from .spot_table import SpotTable
+from .spot_table import SpotTable,SegmentedSpotTable
 from .expression_dataset import ExpressionDataset
 from .image import Image, ImageStack
 from .celltype_mapping import *
-from .spatial_dataset import *
+#from .spatial_dataset import *
 from .util import *
 from . import _version
 __version__ = _version.get_versions()['version']
