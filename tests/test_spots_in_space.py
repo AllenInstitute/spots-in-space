3116585928563612fdfe959a40ed886f6be52671
Brian Long <brianl@alleninstitute.org>
2024-04-02 12:31:39 -0700

    unzip test data

--- tests/test_spots_in_space.py
+++ tests/test_spots_in_space.py
@@ -1,5 +1,24 @@
 from sis import example_function
+from sis import unpack_test_data
+from sis import SpotTable
+import zipfile
+import pathlib
+
+
+
+XENIUM_INTERNAL_NAME = 'output-XETG00044__0025057__Test__20240307__194734'
+
+MERSCOPE_INTERNAL_NAME = '202403080826_1334050571_VMSC21002_region_0'
+
+
 
 
 def test_example_function():
     assert example_function() == 2
+
+
+def test_unpack_test_data():
+    (a,b) = unpack_test_data()
+    print(a)
+    print(b)
+    assert all([a==XENIUM_INTERNAL_NAME, b ==MERSCOPE_INTERNAL_NAME ])
