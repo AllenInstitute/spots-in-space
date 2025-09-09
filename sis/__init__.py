0b5c86baeb9fc1b37ad8bd25b6765426c1e76aa2
Jacob Quon <jacob.quon@alleninstitute.org>
2025-09-09 16:12:52 -0700

    reworked pip installation

--- sis/__init__.py
+++ sis/__init__.py
@@ -2,4 +2,9 @@ from .spot_table import SpotTable,SegmentedSpotTable
 from .image import Image, ImageStack
 from .util import *
 from . import _version
+from . import hpc
+from . import segmentation
+from . import spot_table
+from . import image
+from . import util
 __version__ = _version.get_versions()['version']
